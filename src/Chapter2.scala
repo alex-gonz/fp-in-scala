b380e1380c9086a85ea98d9ec8e355b6e522fcf4
Alex Gonzalez <octet@live.ca>
2016-04-14 08:23:44 -0700

    Finished Chapter 2 problems

--- src/Chapter2.scala
+++ src/Chapter2.scala
@@ -28,7 +28,11 @@ object Chapter2 {
   }
 
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    (a: A) => (b: B) => f(a, b)
+    a => b => f(a, b)
 
-  
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }
